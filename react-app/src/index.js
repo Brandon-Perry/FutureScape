8261708420e44f1affbaff0bdad8b50261396a73
brandon-perry <brandonperryofficial@gmail.com>
2021-01-07 19:53:48 -0500

    React works!

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+
 import './index.css';
 import App from './App';
+import configureStore from './store'
+
+const preloadedState = {
+  test: 'test'
+}
+
+let store = configureStore(preloadedState)
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
