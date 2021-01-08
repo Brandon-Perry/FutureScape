75cb64a7d562d5e138a7b497ebed8f7ecf388434
brandon-perry <brandonperryofficial@gmail.com>
2021-01-08 16:56:44 -0500

    redux store works properly, users persist after login in. Should be ready to merge!

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -8,13 +8,14 @@ const rootReducer = combineReducers({
     session: sessionReducer,
 })
 
+
 let enhancer
 
 if (process.env.NODE_ENV === 'production') {
     enhancer = applyMiddleware(thunk);
 } else {
     const logger = require('redux-logger').default;
-    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancer(applyMiddleware(thunk, logger))
 }
 
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -15,7 +15,7 @@ export const removeUser = () => {
 }
 
 export const authenticate = () => async (dispatch) => {
-    const response = await fetch('/api/auth', {
+    const response = await fetch('/api/auth/', {
         headers: {
             'Content-Type': 'application/json'
         }
