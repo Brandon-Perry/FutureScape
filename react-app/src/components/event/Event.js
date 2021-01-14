f34c1f77a226a2e73ae11a56d3237a87490b8883
brandon-perry <brandonperryofficial@gmail.com>
2021-01-14 13:48:58 -0500

    added display for users and probabilities for prediction, still need to style. Working on user input for predictions.

--- react-app/src/components/event/Event.js
+++ react-app/src/components/event/Event.js
@@ -32,17 +32,40 @@ const Event = () => {
         )
     }
 
-    const displayPredictions = (prediction) => {
-        return (
-            <div className='Event__prediction'>
+    const displayYesPredictions = () => {
+        return eventInfo.predictions.filter(prediction => prediction.choice_id == 1).map((prediction) => {
+            
+            return (
                 <div className='Event__prediction_yes'>
-                    
+                    <p>{prediction.probability}</p>
                 </div>
-                <div className='Event__prediction_no'>
+            )
+            
+        })
+    }
 
+    const displayNoPredictions = () => {
+        return eventInfo.predictions.filter(prediction => prediction.choice_id == 2).map((prediction) => {
+            
+            return (
+                <div className='Event__prediction_no'>
+                    <p>{prediction.probability}</p>
                 </div>
-            </div>
-        )
+            )
+            
+        })
+    }
+
+    const displayUsernamePredictions = () => {
+        return eventInfo.predictions.filter(prediction => prediction.choice_id == 1).map((prediction) => {
+            
+            return (
+                <div className='Event__prediction_username'>
+                    <p>{prediction.users.username}</p>
+                </div>
+            )
+            
+        })
     }
     
     return (
@@ -62,7 +85,19 @@ const Event = () => {
             </div>
 
             <div className='Event__predictions_container'>
+                <div className='Event__prediction_username_container'>
+                    {eventInfo.predictions ? displayUsernamePredictions() : null}
+                </div>
+                <div className='Event__prediction_yes_container'>
+                    {eventInfo.predictions ? displayYesPredictions() : null}
+                </div>
+                <div className='Event__prediction_no_container'>
+                    {eventInfo.predictions ? displayNoPredictions() : null}
+                </div>
+            </div>
 
+            <div className='Event__user_prediction_container'>
+                <input type='range' min='1' max='99' value='50' id='userPrediction'></input>
             </div>
 
             <div className='Event__comments_container'>
