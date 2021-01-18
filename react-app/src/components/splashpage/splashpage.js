4e8105d8a9488404c60e9494245e9122cc4bf4b8
brandon-perry <brandonperryofficial@gmail.com>
2021-01-18 17:57:26 -0500

    Changed a bunch of CSS elements

--- react-app/src/components/splashpage/splashpage.js
+++ react-app/src/components/splashpage/splashpage.js
@@ -29,28 +29,26 @@ const SplashPage = () => {
 
                     <div className='splash-text'>
                         <p classname='about'>
-                            FutureScape is an open source prediction market that uses crowd-sourced
+                            FutureScape is an reputation based prediction market that uses crowd-sourced
                             predictions to create estimates about what will happen in the world. 
                             From global affairs to production numbers, find out what will happen now
                             by signing up today!
                         </p>
                     </div>
 
+                    <div className='splash-form'>
+                        {loginState ? <LoginForm /> : <SignUpForm />}
+                    </div>
                     <div className='splash-buttons'>
-                        <div>
-                            {loginState ? <LoginForm /> : <SignUpForm />}
-                        </div>
                         <div>
                             <button onClick={changeLoginState}>{!loginState ? 'Have an Account Already?' : 'Create an Account'}</button>
                         </div>
+                        <div>
+                            <button>Demo Sign-In</button>
+                        </div>
                     </div>
 
-                    <div>
-                        <p>Or, take a peek</p>
-                    </div>
-                    <div>
-                        <button>Demo Sign-In</button>
-                    </div>
+                   
                 </div>
             </div>
             <SplashFooter />
