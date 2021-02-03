f8d7758ace58fb0c242caf52c7c597e39862c6a0
brandon-perry <brandonperryofficial@gmail.com>
2021-02-02 20:03:44 -0500

    added more links and copyright on splashpage

--- react-app/src/components/splashpage/splashfooter.js
+++ react-app/src/components/splashpage/splashfooter.js
@@ -1,13 +1,25 @@
 import React from 'react';
+// import Date
 
 import './splashpagestyle.css'
 
 const SplashFooter = () => {
+    const currentDate = new Date()
     return (
         <div className='splash-footer'>
-            <div className='splash-footer__github'>
+            <div className='splash-footer__links'>
                <a href='https://github.com/Brandon-Perry/FutureScape'>
-                   <img src={require('../../assets/github_logo.png')} alt={'github logo link'}/></a> 
+                   <img src={require('../../assets/github_logo.png')} alt={'github logo link'}/>
+                </a> 
+                <a href='https://www.linkedin.com/in/brandon-perry/'>
+                    <img src={require('../../assets/linkedin_logo.png')} alt={'linkedin logo link'} />
+                </a>
+                <a href=''>
+                    <img src={require('../../assets/website.png')} alt={'linkedin logo link'} />
+                </a>
+            </div>
+            <div className='splash-footer__copyright'>
+                Copyright {currentDate.getFullYear()}
             </div>
         </div>
     )
